d74c68a64c70b49be962eb6f3e6194710282f031
xuyongzhi <buaaxyz@yeah.net>
2017-09-06 09:09:57 +1000

    Just reallocate the files

--- x_sem_seg/run_ETH_train_with_intensity_nocolor.sh
+++
@@ -1,5 +0,0 @@
-python train_ETH.py --log_dir ETH_log6_small --max_epoch 30 --batch_size 2 --num_point 2048 --no_color_1norm
-
-
-
-#python train_ETH.py --log_dir ETH_log6_small --max_epoch 30 --batch_size 2 --num_point 2048 --no_color_1norm --no_intensity_1norm
