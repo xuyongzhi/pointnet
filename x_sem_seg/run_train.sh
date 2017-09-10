9028444197a7818dc20484d5cbd15711a5f9c3ec
Yongzhi Xu <yx2146@raijin4.(none)>
2017-09-10 23:02:06 +1000

    Just update

--- x_sem_seg/run_train.sh
+++ x_sem_seg/run_train.sh
@@ -1,5 +1,5 @@
 
-python train_sorted.py  --gpu 0    --log_dir log6_4096  --eval_area 6 --max_epoch 50 --batch_size 1024 --num_point 4096   --all_fn_glob stanford_indoor3d_normedh5_stride_0.5_step_1_4096/* 
+python train_sorted.py  --gpu 0    --log_dir log6_4096_office  --eval_area 6 --max_epoch 1 --batch_size 4096 --num_point 4096   --all_fn_glob stanford_indoor3d_normedh5_stride_0.5_step_1_4096/*office*  --train    _data_rate 0.05 --eval_data_rate 0.05
 
 
 
