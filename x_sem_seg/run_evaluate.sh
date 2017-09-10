6695ede1c20768c9b9ac1c90f38f6b4e56af9443
xuyongzhi <buaaxyz@yeah.net>
2017-09-10 22:16:44 +1000

    (1) The h5 data generated by NetProvider is wrong:      The inference result is totally wrong. Maybe the xyz_1norm should be glbal. (2) Holly shit: The inference codes with pre_trained model works fine only only on my own workstation. On NCI and pan's workstation: key ...... not found in checkpoint

--- x_sem_seg/run_evaluate.sh
+++ x_sem_seg/run_evaluate.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #LOG_DIR="log5_4096_bs32"
-LOG_DIR="log1"
+LOG_DIR="LOG_QI/log1"
 echo "LOG_DIR=$LOG_DIR"
 
-#python train_sorted.py --only_evaluate --log_dir $LOG_DIR  --batch_size 4 --num_point 4096 --eval_data_rate 1   --all_fn_glob stanford_indoor3d_normedh5_stride_0.5_step_1_4096/Area_5_conferenceRoom_1     --visu Area_5_conferenceRoom_1
+python train_sorted.py --only_evaluate --log_dir $LOG_DIR  --batch_size 4 --num_point 4096 --eval_data_rate 1   --all_fn_glob stanford_indoor3d_normedh5_stride_0.5_step_1_4096/Area_6_office_22     --visu Area_6_office_22
 
 
 
@@ -14,7 +14,7 @@ echo "LOG_DIR=$LOG_DIR"
 
 
 
-python batch_inference.py --model_path  $LOG_DIR/model.ckpt --dump_dir  $LOG_DIR/dump_eva_spl --output_filelist $LOG_DIR/output_filelist_eva_1.txt --room_data_filelist meta/area6_data_label_1.txt --visu
+#python batch_inference.py --model_path  $LOG_DIR/model.ckpt --dump_dir  $LOG_DIR/dump_eva_spl --output_filelist $LOG_DIR/output_filelist_eva_1.txt --room_data_filelist meta/area6_data_label_1.txt --visu
 
 #python batch_inference.py --model_path log6/model.ckpt --dump_dir log6/split_dump --output_filelist log6/split_output_filelist.txt --room_data_filelist meta/area6_data_split.txt --visu
 
@@ -25,3 +25,19 @@ python batch_inference.py --model_path  $LOG_DIR/model.ckpt --dump_dir  $LOG_DIR
 
 ## ETH_reduced_test_split.txt
 #python batch_inference.py --model_path log6/model.ckpt --dump_dir log6/ETH/reduced_split/dump --output_filelist log6/ETH/reduced_split/output_filelist.txt --room_data_filelist meta/ETH_reduced_test_split.txt --visu --del_column 3
+
+
+
+
+
+
+
+
+
+
+
+#  ('/home/x/Research/pointnet/data/stanford_indoor3d/Area_6_office_22.npy', 'LOG_QI/log1/dump_eva_spl/Area_6_office_22_pred.txt')
+#  file_size= 12
+#  eval mean loss: 0.175759
+#  eval accuracy: 0.928446
+#  all room eval accuracy: 0.928446
